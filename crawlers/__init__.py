5aedcb1fa966ab161ef8cd193818a7401488344c
kinopico <zqmillet@qq.com>
2024-10-11 16:24:55 +0800

    [Chore] add pytest-cov into action workflow.

--- crawlers/__init__.py
+++ crawlers/__init__.py
@@ -1,2 +1,6 @@
 from .chdbits import CHDBits
 from .mteam import MTeam
+
+VERSION = '1.0.0'
+
+__all__ = ['VERSION']
---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+from setuptools import setup
+from setuptools import find_packages
+
+from crawlers import VERSION
+
+with open('crawlers/requirements.txt', 'r', encoding='utf8') as file:
+    install_requires = list(map(lambda x: x.strip(), file.readlines()))
+
+
+setup(
+    name='jmm',
+    version=VERSION,
+    author='kinopico',
+    author_email='zqmillet@qq.com',
+    url='https://github.com/zqmillet/pt-crawler',
+    description='a group of crawlers for private tracker website',
+    packages=find_packages(),
+    install_requires=install_requires,
+)
